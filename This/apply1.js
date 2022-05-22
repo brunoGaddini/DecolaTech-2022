768dd6e356da291fa3c5cca19b2743a8cf55a806
bruno_gaddini <bruno_gaddini@hotmail.com>
2022-05-21 22:07:43 -0300

    https://github.com/brunoGaddini/DecolaTech2022.git

--- This/apply1.js
+++ This/apply1.js
@@ -13,3 +13,16 @@ function getSomeThing() {
 }
 
 getSomeThing.apply(pessoa);
+
+// Segundo exemplo: 
+
+const meuObjeto = {
+    num1: 2,
+    num2: 4,
+};
+
+function soma (a, b) {
+    console.log(this.num1 + this.num2 + a + b);
+}
+
+soma.call(meuObjeto, [1, 5]);
