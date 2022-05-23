18b214ca0bea36ea84e7005a9545381ef9733ca7
bruno_gaddini <bruno_gaddini@hotmail.com>
2022-05-22 22:03:45 -0300

    https://github.com/brunoGaddini/DecolaTech-2022.git

--- Assincrono/Assincrono.js
+++ Assincrono/Assincrono.js
@@ -2,8 +2,17 @@
 
 // Promisse: Objeto de processamento assíncrono. Inicialmente seu valor é desconhecido, portanto pode ser resolvida ou rejeitada. 
 
-const myPromise = new Promise((resolve, reject) => {
-    window.setTimeout(() => {
-        resolve(console.log('Resolvida!'));
-    }, 2000);
+async function resolvePromise(){
+    const myPromise = new Promise((resolve, reject) => {
+        window.setTimeout(() => {
+            resolve(console.log('Resolvida!'));
+        }, 2000);
 });
+
+const resolved = await myPromise
+    .then((result) => result + 'passando pelo then')
+    .then((result) => result + 'e agora acabou')
+    .catch((err) => console.log(err.message));  
+
+    return resolved;
+}
